f9dbbd8d35d56f0db457d06acd1725b80f361ec4
strong-Ting <s8527502987@gmail.com>
2020-09-16 17:41:04 +0800

    sort out

--- INCA_libs/irun.lnx86.11.10.nc/files.ts
+++
@@ -1,2 +0,0 @@
-1599404522 /home/M10912039/GitHub/CIC_2019_univ/testfixture.v
-1599404477 /home/M10912039/GitHub/CIC_2019_univ/CONV_syn.v
---
+++ sim/INCA_libs/irun.lnx86.15.20.nc/.timestamp.ts
@@ -0,0 +1,4 @@
+1600064965 /home/lin/github/CIC_2019_univ/src/CONV.v
+1600248159 /home/lin/github/CIC_2019_univ/sim/testfixture.v
+1553221792 /home/lin/github/CIC_2019_univ/sim/tsmc13_neg.v
+1600248108 /home/lin/github/CIC_2019_univ/syn/CONV_syn.v
